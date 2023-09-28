1c376b1343664b489373443e2434aa74522fb318
Lien2009 <mylien200996@gmail.com>
2023-09-28 18:41:38 +0700

    up

---
+++ SS11-Web Service/practice/demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Customer;
+import com.example.demo.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@Controller + @ResponseBody
+@RequestMapping("/api/customers")
+public class CustomerController {
+    @Autowired
+    private ICustomerService iCustomerService;
+    @GetMapping
+    public ResponseEntity<Iterable<Customer>> findAllCustomer(){
+        List<Customer> customers = (List<Customer>) iCustomerService.findAll();
+        if (customers.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+}
